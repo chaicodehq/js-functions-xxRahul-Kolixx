178da1d0a63c8ac27e8b79457c065c83ca0be140
rahul <rahulkoli11052002@gmail.com>
2026-02-17 21:02:21 +0000

    Chore: Complete 01

--- src/01-dosa-counter.js
+++ src/01-dosa-counter.js
@@ -33,4 +33,31 @@
  */
 export function calculateDosaOrder(type, quantity = 1, isSpicy = false) {
   // Your code here
+  if(typeof type !== 'string' || type.trim().length === 0 || quantity <=0) return null
+
+  let basePrice = 0
+  let pricePerDosa = 0
+
+  switch(type){
+    case "plain": basePrice=40
+                 break;
+    case "masala": basePrice=60
+                   break;
+    case "onion": basePrice=50
+                   break;
+    case "butter": basePrice=70
+                   break;
+    case  "paper": basePrice=90
+                   break;
+    case "cheese": basePrice=80
+                   break;
+    default : return null
+  }
+
+  pricePerDosa = basePrice + (isSpicy?10:0)
+
+  const total = pricePerDosa * quantity
+
+  return {type,quantity,pricePerDosa,total}
+
 }
